4157be654abd6052c1ef5b258e40d7618cfbc302
iwatod <75263827+iwatod@users.noreply.github.com>
2023-10-23 18:36:02 +0300

    c
    c

---
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/SurfaceScreen.kt
@@ -0,0 +1,38 @@
+package com.raywenderlich.android.lab1.screens
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import com.raywenderlich.android.lab1.R
+import com.raywenderlich.android.lab1.router.BackButtonHandler
+import com.raywenderlich.android.lab1.router.FundamentalsRouter
+import com.raywenderlich.android.lab1.router.Screen
+import com.raywenderlich.android.lab1.screenslertDialog.MyColumn
+
+@Composable
+
+fun SurfaceScreen(modifier: Modifier=Modifier){
+    Box(modifier = modifier.fillMaxSize()){
+        MySurface(modifier = modifier.align(Alignment.Center))
+    }
+    BackButtonHandler {
+        FundamentalsRouter.navigateTo(Screen.Navigation)
+    }
+}
+
+@Composable
+fun MySurface(modifier: Modifier){
+Surface(
+    modifier = modifier.size(100.dp),
+    color = Color.LightGray,
+    contentColor = colorResource(id = R.color.colorPrimary),
+    elevation = 1.dp,
+    border = BorderStroke(1.dp, Color.Black)
+){ MyColumn()}
+}
